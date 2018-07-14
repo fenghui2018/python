5a16a0e8c4841ba845216f96fd912161d2f0e460
fenghui2013 <841824090@qq.com>
2018-07-14 14:44:35 +0800

    add lesson_5

---
+++ lesson_5/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
+
+/*
+int main() {
+    printf("hello world");
+    return 0;
+}
+
+int
+main() {
+    printf("hello world");
+    return 0;
+}
+
+
+int
+main()
+{
+    printf("hello world");
+    return 0;
+}
+*/
